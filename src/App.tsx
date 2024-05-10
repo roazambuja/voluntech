7205f46d11bd929a3deb34771a1ebfab49d35c47
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-05-10 17:13:20 -0300

    feat: clean project, define font and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./assets/logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Logo} alt="Logo da aplicação Voluntech" />
       </header>
     </div>
   );
---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  PRIMARY: "#173AEB",
+  PRIMARY_LIGHT: "#76B9F0",
+  PRIMARY_DARK: "#061BB0",
+  SECONDARY: "#FC9642",
+  LIGHT: "#F3F7FA",
+  GREY: "#9E9E9E",
+  BLACK: "#2C2C2C",
+  WHITE: "#FFFFFF",
+};
