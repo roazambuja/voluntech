844ae0e0ac2f2d39769b2fd34a128da6c34873d8
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-06-22 15:49:22 -0300

    feat: define css breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,13 @@
 import { DefaultTheme } from "styled-components";
 import { colors as colorsCode } from "./colors";
+import { breakpoints } from "./breakpoints";
 
 const theme: DefaultTheme = {
   colors: {
     ...colorsCode,
+  },
+  breakpoints: {
+    ...breakpoints,
   },
 };
 
