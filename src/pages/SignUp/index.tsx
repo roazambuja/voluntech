a6b466cdb2b71748d2908e4540340e2a04358de8
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-06-29 22:42:42 -0300

    feat: volunteer steps

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,78 +1,165 @@
-import { BaseSyntheticEvent, useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Stepper } from "../../components/Stepper";
 import { ToggleButton } from "../../components/ToggleButton";
 import { Text, Title, Strong } from "../../styles/global";
-import { Container, CustomPaper, Form, TitleArea } from "./styles";
+import {
+  ButtonArea,
+  CheckIcon,
+  Container,
+  CustomPaper,
+  Form,
+  ProfilePicture,
+  TitleArea,
+  UploadIcon,
+  UserIcon,
+} from "./styles";
 
 function SignUp(): JSX.Element {
   const [currentStep, setCurrentStep] = useState<number>(1);
   const [selectedType, setSelectedType] = useState<string>("Voluntário");
+  const [totalSteps, setTotalSteps] = useState<number>(3);
 
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
 
+  const [cep, setCep] = useState<string>("");
+  const [state, setState] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+
+  const messages = [
+    ["Já possui uma conta? ", <Strong>Faça login</Strong>],
+    "Preencha as informações de localização.",
+    "Selecione sua foto do perfil.",
+    "Seu cadastro foi finalizado com sucesso!",
+  ];
+
   function nextStep(event: BaseSyntheticEvent) {
     event.preventDefault();
     setCurrentStep(currentStep + 1);
-
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
   }
+
+  function previousStep() {
+    setCurrentStep(currentStep - 1);
+  }
+
+  useEffect(() => {
+    selectedType === "Voluntário" ? setTotalSteps(3) : setTotalSteps(5);
+  }, [selectedType]);
 
   return (
     <Container>
       <CustomPaper>
         <TitleArea>
-          <Title>Cadastro</Title>
-          <Text>
-            Já possui uma conta? <Strong>Faça login.</Strong>
-          </Text>
+          {currentStep === totalSteps + 1 ? <CheckIcon /> : <Title>Cadastrar</Title>}
+          <Text>{messages[currentStep - 1]}</Text>
         </TitleArea>
-        <ToggleButton
-          firstTitle="Voluntário"
-          secondTitle="Organização"
-          setSelected={setSelectedType}
-        />
-        <Stepper steps={selectedType == "Voluntário" ? 4 : 5} current={currentStep} />
+        {currentStep === 1 && (
+          <ToggleButton
+            firstTitle="Voluntário"
+            secondTitle="Organização"
+            setSelected={setSelectedType}
+          />
+        )}
+        <Stepper steps={totalSteps} current={currentStep} />
         <Form onSubmit={nextStep}>
-          <Input
-            required
-            id="name"
-            label={selectedType == "Voluntário" ? "Nome" : "Nome da organização"}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            required
-            id="email"
-            type="email"
-            label="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            required
-            id="password"
-            type="password"
-            label="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Input
-            required
-            id="confirmPassword"
-            type="password"
-            label="Confirme sua senha"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <Button type="submit">Avançar</Button>
+          {currentStep === 1 && (
+            <>
+              <Input
+                // required
+                id="name"
+                label={selectedType === "Voluntário" ? "Nome" : "Nome da organização"}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Input
+                // required
+                id="email"
+                type="email"
+                label="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                // required
+                id="password"
+                type="password"
+                label="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Input
+                // required
+                id="confirmPassword"
+                type="password"
+                label="Confirme sua senha"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <Button type="submit">Avançar</Button>
+            </>
+          )}
+
+          {currentStep === 2 && (
+            <>
+              <Input
+                // required
+                id="cep"
+                label={"CEP"}
+                value={cep}
+                onChange={(e) => setCep(e.target.value)}
+              />
+              <Input
+                // required
+                id="state"
+                label="Estado"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+              <Input
+                // required
+                id="city"
+                label="Cidade"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <ButtonArea>
+                <Button type="button" variant="secondary" onClick={previousStep}>
+                  Voltar
+                </Button>
+                <Button type="submit">Avançar</Button>
+              </ButtonArea>
+            </>
+          )}
+
+          {currentStep === 3 && (
+            <>
+              <ProfilePicture>
+                <UserIcon />
+              </ProfilePicture>
+              <Input
+                type="file"
+                accept="image/*"
+                id="picture"
+                label={[<UploadIcon />, "Selecionar foto"]}
+              />
+              <ButtonArea>
+                <Button type="button" variant="secondary" onClick={previousStep}>
+                  Voltar
+                </Button>
+                <Button type="submit">Finalizar Cadastro</Button>
+              </ButtonArea>
+            </>
+          )}
+
+          {currentStep === 4 && (
+            <>
+              <Button type="button">Ir para a página inicial</Button>
+            </>
+          )}
         </Form>
       </CustomPaper>
     </Container>
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Paper } from "../../styles/global";
+import { CheckCircle, Upload, User } from "react-feather";
 
 const Container = styled.div`
   align-items: center;
@@ -30,10 +31,56 @@ const TitleArea = styled.div`
 `;
 
 const Form = styled.form`
+  align-items: center;
   display: flex;
   flex-direction: column;
   gap: 16px;
   width: 100%;
 `;
 
-export { Container, CustomPaper, TitleArea, Form };
+const ButtonArea = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 16px;
+`;
+
+const CheckIcon = styled(CheckCircle)`
+  color: ${(props) => props.theme.colors.SECONDARY};
+  height: 50px;
+  margin: 16px;
+  width: 50px;
+`;
+
+const ProfilePicture = styled.div`
+  width: 80px;
+  height: 80px;
+  background-color: ${(props) => props.theme.colors.PRIMARY_LIGHT};
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const UserIcon = styled(User)`
+  color: ${(props) => props.theme.colors.WHITE};
+  width: 50px;
+  height: 50px;
+`;
+
+const UploadIcon = styled(Upload)`
+  width: 16px;
+  height: 16px;
+  stroke-width: 3px;
+`;
+
+export {
+  Container,
+  CustomPaper,
+  TitleArea,
+  Form,
+  ButtonArea,
+  CheckIcon,
+  ProfilePicture,
+  UserIcon,
+  UploadIcon,
+};
