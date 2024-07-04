d734d03b910f81590d3d1ebd49ffd822a8ab7458
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-04 16:48:16 -0300

    feat: navigate between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
-import SignUp from "./pages/SignUp";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SignUp />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
