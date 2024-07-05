d7994bdf8951dfff7556e32c36440c8bd63ac282
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-04 23:39:38 -0300

    fix: changed Button style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -5,7 +5,7 @@ const Button = styled.button<ButtonProps>`
   border-radius: 4px;
   font-family: "Roboto", sans-serif;
   font-size: 16px;
-  font-weight: bold;
+  font-weight: 500;
   height: 40px;
   width: 100%;
 
@@ -14,7 +14,7 @@ const Button = styled.button<ButtonProps>`
       ? {
           backgroundColor: `${props.theme.colors.WHITE}`,
           color: `${props.theme.colors.SECONDARY}`,
-          border: `2px solid ${props.theme.colors.SECONDARY}`,
+          border: `1px solid ${props.theme.colors.SECONDARY}`,
         }
       : {
           backgroundColor: `${props.theme.colors.PRIMARY}`,
@@ -29,10 +29,6 @@ const Button = styled.button<ButtonProps>`
 
   @media (max-width: ${(props) => props.theme.breakpoints.TABLET}) {
     font-size: 14px;
-  }
-
-  @media (max-width: ${(props) => props.theme.breakpoints.MOBILE}) {
-    border-width: ${(props) => props.variant === "secondary" && "1px"};
   }
 `;
 
