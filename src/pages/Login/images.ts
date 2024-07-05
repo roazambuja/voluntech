d0159f77438ea6b70eb43a7675a862f3eb8abc81
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-04 22:02:01 -0300

    feat: image list

---
+++ src/pages/Login/images.ts
@@ -0,0 +1,23 @@
+export interface ImageProps {
+  link: string;
+  description: string;
+}
+
+export const imageList: ImageProps[] = [
+  {
+    link: "https://plus.unsplash.com/premium_photo-1663127644755-edefeedc2d49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Cachorro recebendo carinho",
+  },
+  {
+    link: "https://plus.unsplash.com/premium_photo-1663099712421-266237bee59b?q=80&w=1826&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Marmita sendo entregue",
+  },
+  {
+    link: "https://images.unsplash.com/photo-1509059852496-f3822ae057bf?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Homem ajudando morador de rua",
+  },
+  {
+    link: "https://images.unsplash.com/photo-1628717341663-0007b0ee2597?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Mulher e menina separando itens em sacolas",
+  },
+];
