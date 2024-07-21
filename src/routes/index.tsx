9231d2c1e04e0dc02e87a106aaeefebac6ad42c0
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-21 20:41:18 -0300

    feat: created introduction page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
+import Introduction from "../pages/Introduction";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Introduction />,
+  },
+  {
+    path: "/login",
     element: <Login />,
   },
   {
