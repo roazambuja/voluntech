ce1d43517688724314116371120e9e312e3a6e12
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-22 21:09:13 -0300

    feat: user register service

---
+++ src/services/users.ts
@@ -0,0 +1,22 @@
+import api from "./api";
+
+interface UserInterface {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
+  role: ["Voluntário" | "Organização"];
+}
+
+interface OrganizationInterface extends UserInterface {
+  cause: string;
+  description: string;
+}
+
+export const postUser = async (body: FormData) => {
+  try {
+    return api.post<{ data: UserInterface | OrganizationInterface }>("/users", body);
+  } catch {
+    throw new Error("Serviço não disponível");
+  }
+};
