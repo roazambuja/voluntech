9f8b37f20e9a0b2389b8d83c7b00d81870c0ceda
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-09 19:41:27 -0300

    feat: dropdown menu

---
+++ src/components/DropdownMenu/index.tsx
@@ -0,0 +1,25 @@
+import { LogOut, User } from "react-feather";
+import { DropdownItem } from "./DropdownItem";
+import { ItemList, DropdownMenu as StyledDropdownMenu } from "./styles";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+interface DropdownMenuProps {
+  open: boolean;
+}
+
+function DropdownMenu({ open }: DropdownMenuProps) {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <StyledDropdownMenu open={open}>
+      <ItemList>
+        <DropdownItem Image={User} text={"Meu Perfil"} action={() => navigate("/perfil")} />
+        <DropdownItem Image={LogOut} text={"Sair"} action={logout} />
+      </ItemList>
+    </StyledDropdownMenu>
+  );
+}
+
+export default DropdownMenu;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import LogoSvg from "../../assets/light-logo.svg";
 import { HeaderContainer, HeaderLogo, MenuIcon } from "./styles";
+import DropdownMenu from "../DropdownMenu";
+import { Link } from "react-router-dom";
 
 function Header(): JSX.Element {
+  const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+
   return (
     <HeaderContainer>
-      <HeaderLogo src={LogoSvg} alt="Logo da aplicação Voluntech" />
-      <MenuIcon />
+      <Link to="/perfil">
+        <HeaderLogo src={LogoSvg} alt="Logo da aplicação Voluntech" />
+      </Link>
+      <MenuIcon onClick={() => setOpenDropdown(!openDropdown)} />
+      <DropdownMenu open={openDropdown} />
     </HeaderContainer>
   );
 }
