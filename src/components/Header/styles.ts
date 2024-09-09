d84fc1fbd7e4adf927315b28e771d4a2917e0116
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-09 19:30:21 -0300

    feat: responsive header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,6 +7,14 @@ const HeaderContainer = styled.header`
   align-items: center;
   padding: 12px 112px;
   background-color: ${(props) => props.theme.colors.PRIMARY};
+
+  @media (max-width: ${(props) => props.theme.breakpoints.TABLET}) {
+    padding: 12px 56px;
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoints.MOBILE}) {
+    padding: 12px 32px;
+  }
 `;
 
 const HeaderLogo = styled.img`
@@ -14,6 +22,7 @@ const HeaderLogo = styled.img`
 `;
 
 const MenuIcon = styled(Menu)`
+  cursor: pointer;
   width: 30px;
   height: 30px;
   color: ${(props) => props.theme.colors.LIGHT};
