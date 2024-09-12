5b43245bd3aade552b0fe9ee3b86badc39aa9ae6
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-11 22:40:09 -0300

    feat: get pix by user service

--- src/services/pix.ts
+++ src/services/pix.ts
@@ -13,6 +13,7 @@ export interface PixInterface {
   _id?: string;
   type: "email" | "phone" | "cpf" | "cnpj" | "random";
   name: string;
+  key: string;
   bank: string;
   organization?: OrganizationInterface;
 }
@@ -20,6 +21,14 @@ export interface PixInterface {
 export const createPixKey = async (payload: PixInterface) => {
   try {
     return api.post("/pix", payload);
+  } catch {
+    throw new Error("Serviço não disponível");
+  }
+};
+
+export const getPixByUser = async (userId: string) => {
+  try {
+    return api.get(`/pix/user/${userId}`);
   } catch {
     throw new Error("Serviço não disponível");
   }
