fefa6dafb1152b50a054e265f4a0405c2e6aeb94
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-11 22:41:09 -0300

    feat: shows pix key in config page

--- src/pages/Organizations/Configurations/index.tsx
+++ src/pages/Organizations/Configurations/index.tsx
@@ -1,11 +1,39 @@
-import { Plus } from "react-feather";
+import { Plus, Edit3 } from "react-feather";
 import { Button } from "../../../components/Button";
 import { Text } from "../../../styles/global";
 import { ConfigSection, CustomPaper as Paper, CustomTitle as Title, TextArea } from "./styles";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../../contexts/AuthContext";
+import { getPixByUser, PixInterface } from "../../../services/pix";
+import { keyTypeLabels } from "../Pix";
 
 function Configurations(): JSX.Element {
   const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const [pixMessage, setPixMessage] = useState<string>();
+  const [pix, setPix] = useState<PixInterface>();
+
+  async function getPix() {
+    try {
+      let { data } = await getPixByUser(user?._id!);
+      if (data.pix) {
+        const pix: PixInterface = data.pix;
+        const pixTypeLabel = keyTypeLabels[pix.type];
+        setPixMessage(`${pixTypeLabel}: ${pix.key}`);
+        setPix(pix);
+      } else {
+        setPixMessage("Não cadastrado.");
+      }
+    } catch {
+      setPixMessage("Não foi possível buscar a chave PIX.");
+    }
+  }
+
+  useEffect(() => {
+    getPix();
+  }, []);
 
   return (
     <Paper>
@@ -22,11 +50,20 @@ function Configurations(): JSX.Element {
       <ConfigSection>
         <TextArea>
           <Title>Chave PIX</Title>
-          <Text>Não cadastrado</Text>
+          <Text>{pixMessage}</Text>
         </TextArea>
         <Button variant="rounded" onClick={() => navigate("/cadastrarPix")}>
-          <Plus strokeWidth={3} />
-          Cadastrar
+          {pix ? (
+            <>
+              <Edit3 strokeWidth={3} />
+              Editar
+            </>
+          ) : (
+            <>
+              <Plus strokeWidth={3} />
+              Cadastrar
+            </>
+          )}
         </Button>
       </ConfigSection>
     </Paper>
