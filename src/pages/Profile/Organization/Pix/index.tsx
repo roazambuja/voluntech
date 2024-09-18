ec6ec254c37a777ce0a9d9a5c69bf98121fb0229
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-17 22:42:33 -0300

    feat: copy key to clipboard area

--- src/pages/Profile/Organization/Pix/index.tsx
+++ src/pages/Profile/Organization/Pix/index.tsx
@@ -9,13 +9,24 @@ interface PixProps {
 }
 
 function Pix({ pix }: PixProps): JSX.Element {
+  function copyKey() {
+    if (pix) {
+      try {
+        navigator.clipboard.writeText(pix.key);
+        alert("Chave PIX copiada para a área de transferência!");
+      } catch (err) {
+        console.error("Falha ao copiar: ", err);
+      }
+    }
+  }
+
   return (
     <>
       {pix && (
         <>
           <Divider text="chave PIX" />
           <PixArea>
-            <CustomText title="Copiar chave PIX">
+            <CustomText onClick={copyKey} title="Copiar chave PIX">
               {pix.type}: {pix.key}
               <CopyIcon />
             </CustomText>
