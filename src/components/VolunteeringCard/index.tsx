57ba1963417d1831af79dc539d77b55e60b3a27b
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-26 17:36:39 -0300

    refactor: volunteering card

--- src/components/VolunteeringCard/index.tsx
+++ src/components/VolunteeringCard/index.tsx
@@ -1,21 +1,7 @@
-import { LiaBroomSolid } from "react-icons/lia";
-import {
-  FaCarSide,
-  FaTruck,
-  FaPaw,
-  FaDonate,
-  FaUtensils,
-  FaBoxOpen,
-  FaHome,
-  FaStethoscope,
-  FaHammer,
-  FaComments,
-  FaHandHoldingHeart,
-  FaTheaterMasks,
-} from "react-icons/fa";
-import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
-import { MoreHorizontal, Heart, BookOpen } from "react-feather";
+import React from "react";
 import { Card, Icon } from "./styles";
+import volunteering, { VolunteeringProps } from "./volunteering";
+import { MoreHorizontal } from "react-feather";
 import { theme } from "../../styles/theme";
 
 interface VolunteeringCardProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -23,67 +9,13 @@ interface VolunteeringCardProps extends React.ButtonHTMLAttributes<HTMLButtonEle
 }
 
 function VolunteeringCard({ title, ...props }: VolunteeringCardProps): JSX.Element {
+  const category: VolunteeringProps | undefined = volunteering.find(
+    (item) => item.category === title
+  );
+
   return (
-    <Card
-      {...props}
-      color={
-        title === "Lar Temporário" || title === "Veterinário"
-          ? `${theme.colors.SECONDARY}`
-          : title === "Logística" ||
-            title === "Transporte" ||
-            title === "Distribuição de materiais" ||
-            title === "Triagem de doações"
-          ? `${theme.colors.PRIMARY}`
-          : title === "Doação Financeira"
-          ? "#0DD746"
-          : title === "Limpeza" || title === "Cozinha" || title === "Trabalho manual"
-          ? "#FF4500"
-          : title === "Apoio psicológico" || title === "Cuidados médicos" || title === "Cuidados"
-          ? "#a321ff"
-          : title === "Aulas" || title === "Comunicação"
-          ? "#0fafac"
-          : title === "Entretenimento"
-          ? "#fc1fba"
-          : `${theme.colors.PRIMARY_DARK}`
-      }
-    >
-      <Icon
-        as={
-          title === "Veterinário"
-            ? FaPaw
-            : title === "Lar Temporário"
-            ? FaHome
-            : title === "Limpeza"
-            ? LiaBroomSolid
-            : title === "Doação Financeira"
-            ? FaDonate
-            : title === "Transporte"
-            ? FaCarSide
-            : title === "Cuidados"
-            ? Heart
-            : title === "Logística"
-            ? FaTruck
-            : title === "Cozinha"
-            ? FaUtensils
-            : title === "Distribuição de materiais"
-            ? FaBoxOpen
-            : title === "Aulas"
-            ? BookOpen
-            : title === "Triagem de doações"
-            ? FaHandHoldingHeart
-            : title === "Apoio psicológico"
-            ? IoChatbubbleEllipsesSharp
-            : title === "Cuidados médicos"
-            ? FaStethoscope
-            : title === "Trabalho manual"
-            ? FaHammer
-            : title === "Comunicação"
-            ? FaComments
-            : title === "Entretenimento"
-            ? FaTheaterMasks
-            : MoreHorizontal
-        }
-      />
+    <Card {...props} color={category?.color || theme.colors.PRIMARY_DARK}>
+      <Icon as={category?.icon || MoreHorizontal} />
       {title}
     </Card>
   );
