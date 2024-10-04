b90822ea904854c28fdc8f34c21afc70ec685676
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-03 21:59:09 -0300

    feat: set loader color

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,8 +1,12 @@
 import { BeatLoader } from "react-spinners";
 import { theme } from "../../styles/theme";
 
-function Loader(): JSX.Element {
-  return <BeatLoader color={theme.colors.PRIMARY_DARK} />;
+interface SpinnerInterface {
+  color?: string;
+}
+
+function Loader({ color }: SpinnerInterface): JSX.Element {
+  return <BeatLoader color={color || theme.colors.PRIMARY_DARK} />;
 }
 
 export { Loader };
