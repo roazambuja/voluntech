14b18bc23cfaf89cbaee8947c1d4386f5bfd2751
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-09 22:59:22 -0300

    feat: participation service

---
+++ src/services/participation.ts
@@ -0,0 +1,25 @@
+import api from "./api";
+import { UserInterface } from "./users";
+import { VolunteeringInterface } from "./volunteering";
+
+export interface ParticipationInterface {
+  _id?: string;
+  user?: UserInterface;
+  volunteering: VolunteeringInterface | string;
+}
+
+export const participate = async (payload: ParticipationInterface) => {
+  try {
+    return api.post("/participation", payload);
+  } catch {
+    throw new Error("Serviço não disponível");
+  }
+};
+
+export const alreadyParticipates = async (volunteering: string) => {
+  try {
+    return api.get(`/participation/${volunteering}`);
+  } catch {
+    throw new Error("Serviço não disponível");
+  }
+};
