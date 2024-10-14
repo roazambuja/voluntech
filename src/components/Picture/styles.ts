93d5576954ca57c4659e7b4d43f0e6d9040d5111
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-13 22:43:48 -0300

    fix: add box sizing only to header variant

--- src/components/Picture/styles.ts
+++ src/components/Picture/styles.ts
@@ -12,7 +12,6 @@ const Picture = styled.div<PictureProps>`
   justify-content: center;
   overflow: auto;
   flex-shrink: 0;
-  box-sizing: border-box;
 
   ${(props) =>
     props.variant === "profile"
@@ -26,6 +25,7 @@ const Picture = styled.div<PictureProps>`
         }
       : {
           width: "100%",
+          boxSizing: "border-box",
         }}
 
   @media (max-width: ${(props) => props.theme.breakpoints.TABLET}) {
