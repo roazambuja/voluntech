276c2001820724047aca05f74c06dbfd0f870be6
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-13 22:44:39 -0300

    feat: handle pictures upload

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -5,11 +5,35 @@ import { Button } from "../../components/Button";
 import { Picture } from "../../components/Picture";
 import { Input } from "../../components/Input";
 import { useState } from "react";
+import { UploadedImages } from "./styles";
 
 function Post(): JSX.Element {
   const navigate = useNavigate();
-  // const [images, setImages] =
   const [text, setText] = useState<string>();
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [images, setImages] = useState<File[]>([]);
+
+  function handleUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+    if (files) {
+      const fileArray = Array.from(files);
+      setImages((prevImages) => [...prevImages, ...fileArray]);
+
+      const newImageUrls = fileArray.map((file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        return new Promise<string>((resolve) => {
+          reader.onloadend = () => {
+            resolve(reader.result as string);
+          };
+        });
+      });
+
+      Promise.all(newImageUrls).then((urls) => {
+        setImageUrls((prevUrls) => [...prevUrls, ...urls]);
+      });
+    }
+  }
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -27,14 +51,21 @@ function Post(): JSX.Element {
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <Picture variant="header" />
+        <UploadedImages>
+          {imageUrls.length === 0 ? (
+            <Picture variant="header" />
+          ) : (
+            imageUrls.map((url, index) => <Picture key={index} variant="profile" src={url} />)
+          )}
+        </UploadedImages>
         <Input
           type="file"
           label="Adicionar imagens"
           accept="image/*"
           id="pictures"
           width="full"
-          // onChange={handleUpload}
+          multiple
+          onChange={handleUpload}
         />
         <Button type="submit">Publicar</Button>
       </Form>
