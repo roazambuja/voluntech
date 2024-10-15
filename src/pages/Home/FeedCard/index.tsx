f381301813070f910405f82e665a419fccb23f21
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-14 21:33:45 -0300

    feat: created post card

--- src/pages/Home/FeedCard/index.tsx
+++ src/pages/Home/FeedCard/index.tsx
@@ -11,6 +11,7 @@ import volunteeringList, {
   VolunteeringProps,
 } from "../../../components/VolunteeringCard/volunteering";
 import { useEffect, useState } from "react";
+import Carousel from "../../../components/Carousel";
 
 export interface FeedCardProps {
   data: UpdatesInterface;
@@ -18,11 +19,12 @@ export interface FeedCardProps {
 
 function FeedCard({ data }: FeedCardProps): JSX.Element {
   const isProject = data.type === "project";
+  const isVolunteering = data.type === "volunteering";
   const [category, setCategory] = useState<VolunteeringProps | undefined>();
 
   useEffect(() => {
     {
-      data.type === "volunteering" &&
+      isVolunteering &&
         setCategory(
           volunteeringList.find((item: VolunteeringProps) => item.category === data.category)
         );
@@ -32,7 +34,7 @@ function FeedCard({ data }: FeedCardProps): JSX.Element {
   return (
     <>
       <Paper>
-        {!isProject && (
+        {isVolunteering && (
           <VolunteeringHeader color={category?.color || theme.colors.PRIMARY_DARK}>
             <Icon as={category?.icon || MoreHorizontal} />
           </VolunteeringHeader>
@@ -55,12 +57,19 @@ function FeedCard({ data }: FeedCardProps): JSX.Element {
           />
           <TextArea>
             <Title>{isProject ? data.organization.name : data.project.organization.name}</Title>
-            <Text>Cadastrou um novo {isProject ? "projeto" : "voluntariado"}</Text>
+            <Text>
+              {isProject || isVolunteering
+                ? "Cadastrou um novo " + (isProject ? "projeto" : "voluntariado")
+                : `Fez uma postagem no projeto ${data.project.title}`}
+            </Text>
           </TextArea>
         </UpdateHeader>
         <DescriptionArea>
-          <Title>{isProject ? data.title : data.category}</Title>
-          <Text>{data.description}</Text>
+          <Title>{isProject ? data.title : isVolunteering && data.category}</Title>
+          <Text>{isProject || isVolunteering ? data.description : data.text}</Text>
+          {data.type === "post" && data.pictures && data.pictures.length > 0 && (
+            <Carousel pictures={data.pictures} />
+          )}
         </DescriptionArea>
       </Paper>
     </>
