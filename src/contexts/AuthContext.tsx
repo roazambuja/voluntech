33a5fcdbe2c02344795f2b3d1020523074776c48
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-23 19:55:07 -0300

    feat: login as visitor

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,15 @@
 import React, { createContext, useState, useContext, ReactNode, useEffect } from "react";
 import api from "../services/api";
-import { getLoggedUser, OrganizationInterface, UserInterface } from "../services/users";
+import {
+  getLoggedUser,
+  OrganizationInterface,
+  UserInterface,
+  VisitorInterface,
+} from "../services/users";
 
 interface AuthContextData {
-  user: null | UserInterface | OrganizationInterface;
-  login: (token: string) => void;
+  user: null | UserInterface | OrganizationInterface | VisitorInterface;
+  login: (token?: string) => void;
   logout: () => void;
   loading: boolean;
 }
@@ -16,40 +21,56 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<null | UserInterface | OrganizationInterface>(null);
+  const [user, setUser] = useState<null | UserInterface | OrganizationInterface | VisitorInterface>(
+    null
+  );
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      login(token);
+      if (token === "Visitante") {
+        login();
+      } else {
+        login(token);
+      }
     } else {
       setLoading(false);
     }
   }, []);
 
-  async function login(token: string): Promise<string | undefined> {
+  async function login(token?: string): Promise<string | undefined> {
     setLoading(true);
-    api.defaults.headers["Authorization"] = `Bearer ${token}`;
+    if (token) {
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
-    try {
-      let { data } = await getLoggedUser();
-      if (!localStorage.getItem("token")) {
-        localStorage.setItem("token", token);
+      try {
+        let { data } = await getLoggedUser();
+        if (!localStorage.getItem("token")) {
+          localStorage.setItem("token", token);
+        }
+        setUser(data.user);
+        return data.user._id;
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
       }
-      setUser(data.user);
-      return data.user._id;
-    } catch (error) {
-      console.log(error);
-    } finally {
+    } else {
+      setUser({ role: "Visitante" });
       setLoading(false);
+      if (!localStorage.getItem("token")) {
+        localStorage.setItem("token", "Visitante");
+      }
     }
   }
 
   function logout() {
     setUser(null);
-    localStorage.removeItem("token");
-    api.defaults.headers.Authorization = "";
+    if (user?.role !== "Visitante") {
+      localStorage.removeItem("token");
+      api.defaults.headers.Authorization = "";
+    }
   }
 
   return (
