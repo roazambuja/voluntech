c13fc379dd60e249d74b9f2e423fd84fbc2c2696
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-30 23:27:27 -0300

    feat: send and get messages services

---
+++ src/services/message.ts
@@ -0,0 +1,25 @@
+import api from "./api";
+import { OrganizationInterface, UserInterface } from "./users";
+
+export interface MessageInterface {
+  _id?: string;
+  to: UserInterface | OrganizationInterface;
+  from: UserInterface | OrganizationInterface;
+  content: string;
+}
+
+export const sendMessage = async (payload: MessageInterface) => {
+  try {
+    return api.post("/message", payload);
+  } catch {
+    throw new Error("Serviço não disponível");
+  }
+};
+
+export const getMessages = async (id: string) => {
+  try {
+    return api.get(`/message/${id}`);
+  } catch {
+    throw new Error("Serviço não disponível");
+  }
+};
